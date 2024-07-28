caa2abab8e4644be4ec7dc4de70b7d953e384046
N.Dubskiy <167004436+DonaldMcGallavry@users.noreply.github.com>
2024-07-28 15:50:12 +0300

    Add files via upload

---
+++ ITMO.WFapp.Lab02.FourthTask/ParentForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace ITMO.WFapp.Lab02.FourthTask
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+
+        public ParentForm()
+        {
+            InitializeComponent();
+            // Свойству Text панели spData устанавливается текущая дата
+            
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+         
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            
+            spWin.Text = "Windows is cascade";
+            spWin.Text = "Windows is horizontal";
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+    }
+}
